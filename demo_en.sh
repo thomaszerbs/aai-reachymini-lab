dd7a1f20480d65b83f4e697851cfe0dbbd88f38b
alexhegit <31022192+alexhegit@users.noreply.github.com>
2026-05-13 10:10:12 +0800

    Update demo_en.sh

--- demo_en.sh
+++ demo_en.sh
@@ -1,1 +1,1 @@
-python emo_v9.py --piper-model ./models/en-us-blizzard_lessac-medium.onnx --model qwen3.5:0.8b --gentle --asr
+python emo_v8.py --piper-model ./models/en-us-blizzard_lessac-medium.onnx --model qwen3:0.6b --gentle --asr
